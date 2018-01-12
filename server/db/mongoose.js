5b46cb8671a337c2df1cc29d0ea320d27f262eab
Boyko Dimitrov <bdimitrov@westum.com>
2018-01-12 10:22:37 +0200

    heroku changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://batboy4e:admin@ds251747.mlab.com:51747/node-rest-api-course-2018' || 'mongodb://localhost:27017/TodoApp');
 module.exports= {mongoose};
